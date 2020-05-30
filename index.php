f3960bafb1ac34891b68a789c06a7b4c45d7dad6
Muhamar <amararrasyid@gmail.com>
2020-05-30 20:56:02 +0700

    Tugas PHP 8
    Mengerjakan Tugas Niomic

---
+++ index.php
@@ -0,0 +1,512 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>tugas 8</title>
+</head>
+
+
+<body>
+    <table>
+        <tr>
+            <td>
+                <?php
+
+                for ($a = 10; $a > 0; $a--) {
+                    for ($i = 1; $i <= $a; $i++) {
+                        echo "_";
+                    }
+                    for ($a1 = 10; $a1 > $a; $a1--) {
+                        echo "x";
+                    }
+                    for ($a2 = 10; $a2 > $a; $a2--) {
+                        echo "x";
+                    }
+                    echo "<br>";
+                }
+                for ($b = 0; $b <= 10; $b++) {
+                    for ($j = 1; $j <= $b; $j++) {
+                        echo "_";
+                    }
+                    for ($b1 = 10; $b1 > $b; $b1--) {
+                        echo "x";
+                    }
+                    for ($b2 = 10; $b2 > $b; $b2--) {
+                        echo "x";
+                    }
+                    echo "<br>";
+                }
+                ?>
+            </td>
+            <td>
+                <?php
+
+                for ($a = 10; $a > 0; $a--) {
+                    for ($i = 1; $i <= $a; $i++) {
+                        echo "_";
+                    }
+                    for ($a1 = 10; $a1 > $a; $a1--) {
+                        echo "x";
+                    }
+                    for ($a2 = 10; $a2 > $a; $a2--) {
+                        echo "x";
+                    }
+                    echo "<br>";
+                }
+                for ($b = 0; $b <= 10; $b++) {
+                    for ($j = 1; $j <= $b; $j++) {
+                        echo "_";
+                    }
+                    for ($b1 = 10; $b1 > $b; $b1--) {
+                        echo "x";
+                    }
+                    for ($b2 = 10; $b2 > $b; $b2--) {
+                        echo "x";
+                    }
+                    echo "<br>";
+                }
+
+
+                ?>
+            </td>
+            <td>
+                <?php
+
+                for ($a = 10; $a > 0; $a--) {
+                    for ($i = 1; $i <= $a; $i++) {
+                        echo "_";
+                    }
+                    for ($a1 = 10; $a1 > $a; $a1--) {
+                        echo "x";
+                    }
+                    for ($a2 = 10; $a2 > $a; $a2--) {
+                        echo "x";
+                    }
+                    echo "<br>";
+                }
+                for ($b = 0; $b <= 10; $b++) {
+                    for ($j = 1; $j <= $b; $j++) {
+                        echo "_";
+                    }
+                    for ($b1 = 10; $b1 > $b; $b1--) {
+                        echo "x";
+                    }
+                    for ($b2 = 10; $b2 > $b; $b2--) {
+                        echo "x";
+                    }
+                    echo "<br>";
+                }
+
+
+                ?>
+            </td>
+            <td>
+                <?php
+
+                for ($a = 10; $a > 0; $a--) {
+                    for ($i = 1; $i <= $a; $i++) {
+                        echo "_";
+                    }
+                    for ($a1 = 10; $a1 > $a; $a1--) {
+                        echo "x";
+                    }
+                    for ($a2 = 10; $a2 > $a; $a2--) {
+                        echo "x";
+                    }
+                    echo "<br>";
+                }
+                for ($b = 0; $b <= 10; $b++) {
+                    for ($j = 1; $j <= $b; $j++) {
+                        echo "_";
+                    }
+                    for ($b1 = 10; $b1 > $b; $b1--) {
+                        echo "x";
+                    }
+                    for ($b2 = 10; $b2 > $b; $b2--) {
+                        echo "x";
+                    }
+                    echo "<br>";
+                }
+
+
+                ?>
+            </td>
+            <td>
+                <?php
+
+                for ($a = 10; $a > 0; $a--) {
+                    for ($i = 1; $i <= $a; $i++) {
+                        echo "_";
+                    }
+                    for ($a1 = 10; $a1 > $a; $a1--) {
+                        echo "x";
+                    }
+                    for ($a2 = 10; $a2 > $a; $a2--) {
+                        echo "x";
+                    }
+                    echo "<br>";
+                }
+                for ($b = 0; $b <= 10; $b++) {
+                    for ($j = 1; $j <= $b; $j++) {
+                        echo "_";
+                    }
+                    for ($b1 = 10; $b1 > $b; $b1--) {
+                        echo "x";
+                    }
+                    for ($b2 = 10; $b2 > $b; $b2--) {
+                        echo "x";
+                    }
+                    echo "<br>";
+                }
+
+
+                ?>
+            </td>
+            <td>
+                <?php
+
+                for ($a = 10; $a > 0; $a--) {
+                    for ($i = 1; $i <= $a; $i++) {
+                        echo "_";
+                    }
+                    for ($a1 = 10; $a1 > $a; $a1--) {
+                        echo "x";
+                    }
+                    for ($a2 = 10; $a2 > $a; $a2--) {
+                        echo "x";
+                    }
+                    echo "<br>";
+                }
+                for ($b = 0; $b <= 10; $b++) {
+                    for ($j = 1; $j <= $b; $j++) {
+                        echo "_";
+                    }
+                    for ($b1 = 10; $b1 > $b; $b1--) {
+                        echo "x";
+                    }
+                    for ($b2 = 10; $b2 > $b; $b2--) {
+                        echo "x";
+                    }
+                    echo "<br>";
+                }
+
+
+                ?>
+            </td>
+            <td>
+                <?php
+
+                for ($a = 10; $a > 0; $a--) {
+                    for ($i = 1; $i <= $a; $i++) {
+                        echo "_";
+                    }
+                    for ($a1 = 10; $a1 > $a; $a1--) {
+                        echo "x";
+                    }
+                    for ($a2 = 10; $a2 > $a; $a2--) {
+                        echo "x";
+                    }
+                    echo "<br>";
+                }
+                for ($b = 0; $b <= 10; $b++) {
+                    for ($j = 1; $j <= $b; $j++) {
+                        echo "_";
+                    }
+                    for ($b1 = 10; $b1 > $b; $b1--) {
+                        echo "x";
+                    }
+                    for ($b2 = 10; $b2 > $b; $b2--) {
+                        echo "x";
+                    }
+                    echo "<br>";
+                }
+
+
+                ?>
+            </td>
+            <td>
+                <?php
+
+                for ($a = 10; $a > 0; $a--) {
+                    for ($i = 1; $i <= $a; $i++) {
+                        echo "_";
+                    }
+                    for ($a1 = 10; $a1 > $a; $a1--) {
+                        echo "x";
+                    }
+                    for ($a2 = 10; $a2 > $a; $a2--) {
+                        echo "x";
+                    }
+                    echo "<br>";
+                }
+                for ($b = 0; $b <= 10; $b++) {
+                    for ($j = 1; $j <= $b; $j++) {
+                        echo "_";
+                    }
+                    for ($b1 = 10; $b1 > $b; $b1--) {
+                        echo "x";
+                    }
+                    for ($b2 = 10; $b2 > $b; $b2--) {
+                        echo "x";
+                    }
+                    echo "<br>";
+                }
+
+
+                ?>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <?php
+
+                for ($a = 10; $a > 0; $a--) {
+                    for ($i = 1; $i <= $a; $i++) {
+                        echo "_";
+                    }
+                    for ($a1 = 10; $a1 > $a; $a1--) {
+                        echo "x";
+                    }
+                    for ($a2 = 10; $a2 > $a; $a2--) {
+                        echo "x";
+                    }
+                    echo "<br>";
+                }
+                for ($b = 0; $b <= 10; $b++) {
+                    for ($j = 1; $j <= $b; $j++) {
+                        echo "_";
+                    }
+                    for ($b1 = 10; $b1 > $b; $b1--) {
+                        echo "x";
+                    }
+                    for ($b2 = 10; $b2 > $b; $b2--) {
+                        echo "x";
+                    }
+                    echo "<br>";
+                }
+                ?>
+            </td>
+            <td>
+                <?php
+
+                for ($a = 10; $a > 0; $a--) {
+                    for ($i = 1; $i <= $a; $i++) {
+                        echo "_";
+                    }
+                    for ($a1 = 10; $a1 > $a; $a1--) {
+                        echo "x";
+                    }
+                    for ($a2 = 10; $a2 > $a; $a2--) {
+                        echo "x";
+                    }
+                    echo "<br>";
+                }
+                for ($b = 0; $b <= 10; $b++) {
+                    for ($j = 1; $j <= $b; $j++) {
+                        echo "_";
+                    }
+                    for ($b1 = 10; $b1 > $b; $b1--) {
+                        echo "x";
+                    }
+                    for ($b2 = 10; $b2 > $b; $b2--) {
+                        echo "x";
+                    }
+                    echo "<br>";
+                }
+
+
+                ?>
+            </td>
+            <td>
+                <?php
+
+                for ($a = 10; $a > 0; $a--) {
+                    for ($i = 1; $i <= $a; $i++) {
+                        echo "_";
+                    }
+                    for ($a1 = 10; $a1 > $a; $a1--) {
+                        echo "x";
+                    }
+                    for ($a2 = 10; $a2 > $a; $a2--) {
+                        echo "x";
+                    }
+                    echo "<br>";
+                }
+                for ($b = 0; $b <= 10; $b++) {
+                    for ($j = 1; $j <= $b; $j++) {
+                        echo "_";
+                    }
+                    for ($b1 = 10; $b1 > $b; $b1--) {
+                        echo "x";
+                    }
+                    for ($b2 = 10; $b2 > $b; $b2--) {
+                        echo "x";
+                    }
+                    echo "<br>";
+                }
+
+
+                ?>
+            </td>
+            <td>
+                <?php
+
+                for ($a = 10; $a > 0; $a--) {
+                    for ($i = 1; $i <= $a; $i++) {
+                        echo "_";
+                    }
+                    for ($a1 = 10; $a1 > $a; $a1--) {
+                        echo "x";
+                    }
+                    for ($a2 = 10; $a2 > $a; $a2--) {
+                        echo "x";
+                    }
+                    echo "<br>";
+                }
+                for ($b = 0; $b <= 10; $b++) {
+                    for ($j = 1; $j <= $b; $j++) {
+                        echo "_";
+                    }
+                    for ($b1 = 10; $b1 > $b; $b1--) {
+                        echo "x";
+                    }
+                    for ($b2 = 10; $b2 > $b; $b2--) {
+                        echo "x";
+                    }
+                    echo "<br>";
+                }
+
+
+                ?>
+            </td>
+            <td>
+                <?php
+
+                for ($a = 10; $a > 0; $a--) {
+                    for ($i = 1; $i <= $a; $i++) {
+                        echo "_";
+                    }
+                    for ($a1 = 10; $a1 > $a; $a1--) {
+                        echo "x";
+                    }
+                    for ($a2 = 10; $a2 > $a; $a2--) {
+                        echo "x";
+                    }
+                    echo "<br>";
+                }
+                for ($b = 0; $b <= 10; $b++) {
+                    for ($j = 1; $j <= $b; $j++) {
+                        echo "_";
+                    }
+                    for ($b1 = 10; $b1 > $b; $b1--) {
+                        echo "x";
+                    }
+                    for ($b2 = 10; $b2 > $b; $b2--) {
+                        echo "x";
+                    }
+                    echo "<br>";
+                }
+
+
+                ?>
+            </td>
+            <td>
+                <?php
+
+                for ($a = 10; $a > 0; $a--) {
+                    for ($i = 1; $i <= $a; $i++) {
+                        echo "_";
+                    }
+                    for ($a1 = 10; $a1 > $a; $a1--) {
+                        echo "x";
+                    }
+                    for ($a2 = 10; $a2 > $a; $a2--) {
+                        echo "x";
+                    }
+                    echo "<br>";
+                }
+                for ($b = 0; $b <= 10; $b++) {
+                    for ($j = 1; $j <= $b; $j++) {
+                        echo "_";
+                    }
+                    for ($b1 = 10; $b1 > $b; $b1--) {
+                        echo "x";
+                    }
+                    for ($b2 = 10; $b2 > $b; $b2--) {
+                        echo "x";
+                    }
+                    echo "<br>";
+                }
+
+
+                ?>
+            </td>
+            <td>
+                <?php
+
+                for ($a = 10; $a > 0; $a--) {
+                    for ($i = 1; $i <= $a; $i++) {
+                        echo "_";
+                    }
+                    for ($a1 = 10; $a1 > $a; $a1--) {
+                        echo "x";
+                    }
+                    for ($a2 = 10; $a2 > $a; $a2--) {
+                        echo "x";
+                    }
+                    echo "<br>";
+                }
+                for ($b = 0; $b <= 10; $b++) {
+                    for ($j = 1; $j <= $b; $j++) {
+                        echo "_";
+                    }
+                    for ($b1 = 10; $b1 > $b; $b1--) {
+                        echo "x";
+                    }
+                    for ($b2 = 10; $b2 > $b; $b2--) {
+                        echo "x";
+                    }
+                    echo "<br>";
+                }
+
+
+                ?>
+            </td>
+            <td>
+                <?php
+
+                for ($a = 10; $a > 0; $a--) {
+                    for ($i = 1; $i <= $a; $i++) {
+                        echo "_";
+                    }
+                    for ($a1 = 10; $a1 > $a; $a1--) {
+                        echo "x";
+                    }
+                    for ($a2 = 10; $a2 > $a; $a2--) {
+                        echo "x";
+                    }
+                    echo "<br>";
+                }
+                for ($b = 0; $b <= 10; $b++) {
+                    for ($j = 1; $j <= $b; $j++) {
+                        echo "_";
+                    }
+                    for ($b1 = 10; $b1 > $b; $b1--) {
+                        echo "x";
+                    }
+                    for ($b2 = 10; $b2 > $b; $b2--) {
+                        echo "x";
+                    }
+                    echo "<br>";
+                }
+
+
+                ?>
+            </td>
+        </tr>
+    </table>
+</body>
+
+</html>
